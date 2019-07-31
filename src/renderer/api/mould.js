31d764265bb3bcd556c2d42fabf560c711d45dc8
cyir <2504532497@qq.com>
2019-07-31 21:07:28 +0800

    添加模板菜单 完成模板修改 123

--- src/renderer/api/mould.js
+++ src/renderer/api/mould.js
@@ -1,7 +1,7 @@
 import request from '../utils/request'
 
 export function getMenuList() {
-    return request.get("/sys/templatefield/querymenulist")
+    return request.post("/sys/templatefield/querymenulist")
 }
 export function getEmp() {
     return request.get("/sys/dept/list")
