ca1c5d99a677e60eafb719697ad42c5bd1ff669c
cysir <2504532497@qq.com>
2019-08-14 21:30:33 +0800

    添加模板菜单 完成模板修改 模板页面搞定 角色管理完成

--- src/renderer/page/role/Index.vue
+++ src/renderer/page/role/Index.vue
@@ -25,7 +25,8 @@
                         <FormItem label="拥有菜单" prop="deptId">
                             <Tree ref="o_menu" :data="roleForm.menuIdListTree" show-checkbox></Tree>
                         </FormItem>
-                        <Button @click="saveRole" style="width: 100px;margin:0 auto;display: block" type="primary">添加角色</Button>
+                        <Button v-if="!isUpdate" @click="saveRole" style="width: 100px;margin:0 auto;display: block" type="primary">添加角色</Button>
+                        <Button v-if="isUpdate" @click="updateRole" style="width: 100px;margin:0 auto;display: block" type="primary">修改角色</Button>
                     </Form>
                 </div>
             </Modal>
@@ -40,13 +41,16 @@
 <script>
     import roleApi from '../../api/role'
     const TAG = '角色管理'
+    var _this = null
     export default {
         name: "Index",
         data(){
             return {
+                isUpdate:false,
                 roleForm:{
                     roleName:'',
                     deptId:'',
+                    roleId:'',
                     remark:'',
                     deptList:[],
                     menuIdListTree:[{
@@ -83,7 +87,11 @@
                     title:'操作',
                         render:(h,params)=>{
                         let _this = this;
-                        return h('div',[h('Button',{props:{type:'success',size:'small'},style:{'margin-right':'2px'}},'查看'),h('Button',{props:{type:'error',size:'small'},on:{
+                        return h('div',[h('Button',{props:{type:'success',size:'small'},style:{'margin-right':'2px'},on:{
+                            click(){
+                                _this.viewRole(params.row.roleId);
+                            }
+                            }},'查看'),h('Button',{props:{type:'error',size:'small'},on:{
                             click(){
                                 _this.$Modal.confirm({content:'确定删除该角色吗?',onOk(){
                                     roleApi.deleteRoleApi([params.row.roleId]).then(resp=>{
@@ -101,9 +109,10 @@
         },
         mounted() {
             this.loadRoles();
-
+            _this = this;
         },
         methods:{
+            //创建角色
             saveRole(){
                 console.log('角色表单',this.roleForm);
                 let dept = this.$refs.o_dept.getCheckedNodes()
@@ -134,7 +143,45 @@
                     console.log('角色创建失败')
                 })
             },
+            //创建角色
+            updateRole(){
+                console.log('角色表单',this.roleForm);
+                let dept = this.$refs.o_dept.getCheckedNodes()
+                let menu = this.$refs.o_menu.getCheckedNodes()
+                let menuIdList = menu.map((value)=>{
+                    if (value.title == '全部'){
+                        return -1;
+                    }
+                    return value.id;
+                });
+                let deptIdList = dept.map((value)=>{
+                    if (value.title == '全部'){
+                        return -1;
+                    }
+                    return value.id;
+                });
+                console.log('更新角色,>>>>>>>>',menuIdList,deptIdList);
+                if (menuIdList.findIndex(v=>{return v == -1})!=-1){
+                    menuIdList.splice(menuIdList.findIndex(value => {return value == -1}),1)
+                }
+                if (deptIdList.findIndex(v=>{return v == -1})!=-1){
+                    deptIdList.splice(deptIdList.findIndex(value => {return value == -1}),1)
+                }
+
+
+                roleApi.updateRoleApi({roleId:this.roleForm.roleId,roleName:this.roleForm.roleName,deptId:this.roleForm.deptId,remark:this.roleForm.remark,
+                    menuIdList,deptIdList
+                }).then(resp=>{
+                    console.log('角色修改成功');
+                    this.addRole = false;
+                    this.loadRoles();
+                }).catch(error=>{
+                    console.log('角色修改失败')
+                })
+            },
+            //添加角色
             async openAddRole(){
+                this.isUpdate = false;
                 this.addRole = true;
                 let dept = await roleApi.selectDept();
                 let menu = await roleApi.selectMenu();
@@ -148,7 +195,65 @@
                    return {title:value.name,id:value.menuId};
                 });
                 this.roleForm.menuIdListTree[0].children=m;
-                // console.log('asdsad',t)
+                this.roleForm.remark = '';
+                this.roleForm.deptId = '';
+                this.roleForm.roleId = '';
+                this.roleForm.roleName = '';
+            },
+            //查看角色权限
+            async viewRole(id){
+                this.addRole = true;
+                let depts = await roleApi.selectDept();
+                let menus = await roleApi.selectMenu();
+                this.roleForm.deptList = depts.deptList;
+                let t = depts.deptList.map((value,index)=>{
+                    // console.log('aaaa',value.name)
+                    return {title:value.name,id:value.deptId}
+                });
+                this.roleForm.deptIdListTree[0].children=t;
+                let m = menus.menuList.map((value,index)=>{
+                    return {title:value.name,id:value.menuId};
+                });
+                this.roleForm.menuIdListTree[0].children=m;
+                //aaaaaaaaaa
+                this.isUpdate = true;
+                let resp = await roleApi.roleInfoApi(id);
+                console.log('查询角色信息',resp);
+                let role = resp.role;
+                this.roleForm.remark = role.remark;
+                this.roleForm.deptId = role.deptId;
+                this.roleForm.roleId = role.roleId;
+                this.roleForm.roleName = role.roleName;
+                console.log(this.roleForm);
+                let dept = role.deptList;
+                let menu = role.menuList;
+            //    设置默认选中
+                this.roleForm.deptIdListTree[0].children = this.roleForm.deptIdListTree[0].children.map((value,index)=>{
+                    if (dept.findIndex(v=>{
+                        if (v == null)
+                            return false;
+                        return v.deptId == value.id
+                    })!=-1){
+                        console.log('找到',value,dept)
+                        return {title:value.title,id:value.id,checked:true}
+                    }
+                    console.log('重新》》',value)
+                    return {title:value.title,id:value.id,checked:false}
+                });
+                //设置菜单默认选中
+                this.roleForm.menuIdListTree[0].children = this.roleForm.menuIdListTree[0].children.map((value,index)=>{
+                    if (dept.findIndex(v=>{
+                        console.log('aaa>',v,'  bbb>',value)
+                        if (v == null)
+                            return
+                        return v.menuId == value.id
+                    })!=-1){
+                        console.log('找到')
+                        return {title:value.title,id:value.id,checked:true}
+                    }
+                    console.log('重新》》',value)
+                    return {title:value.title,id:value.id,checked:false}
+                });
             },
             loadRoles(){
 
